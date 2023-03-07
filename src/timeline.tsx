e88312e301ca535438ea8c01deab6caeebe23ab6
Wesley Lancel <wesleylancel@gmail.com>
2023-03-07 21:18:03 +0100

    Remove CSS import

--- src/timeline.tsx
+++ src/timeline.tsx
@@ -2,7 +2,6 @@ import React, { Component } from 'react';
 import { difference, intersection, each, omit, keys } from 'lodash';
 import type { CustomTime, SelectionOptions, TimelineEventsHandlers, TimelineEventsWithMissing } from './models';
 import { DateType, IdType, Timeline as VisTimeline, TimelineGroup, TimelineItem, TimelineOptions } from 'vis-timeline';
-import 'vis-timeline/styles/vis-timeline-graph2d.min.css';
 
 const events: TimelineEventsWithMissing[] = [
 	'currentTimeTick',
